2819c555bb92f347e3d099b6338e43fadc7feb49
Zaman <Sunny.Zaman@ontario.ca>
2020-06-07 20:03:06 -0400

    game loader

---
+++ Scripts/GameLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour {
+
+	void Start () {
+		if (PlayerPrefs.HasKey ("Tutorial")) {
+			SceneManager.LoadScene ("Menu Scene");
+		}
+		else {
+			PlayerPrefs.SetInt ("Tutorial", 1);
+			SceneManager.LoadScene ("Tutorial Scene");
+		}
+
+		if (PlayerPrefs.HasKey ("Initalize Soccer") && PlayerPrefs.HasKey ("Initalize Basketball")) {
+			//Do Nothing
+		} else {
+			PlayerPrefs.SetInt ("Initalize Soccer", 1);
+			PlayerPrefs.SetInt ("Initalize Basketball", 1);
+		}
+		
+	}
+
+}
